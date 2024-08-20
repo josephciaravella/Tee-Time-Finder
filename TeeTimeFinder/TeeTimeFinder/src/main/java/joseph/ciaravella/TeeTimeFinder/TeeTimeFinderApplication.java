4f447be26e1e142b2386bf957f5390ab88c6bb61
Joseph Ciaravella <joseph.ciaravella@gmail.com>
2024-08-19 20:26:49 -0400

    initial build + dependencies added in build.gradle + and application properties specified in application.properties + db created + model imported (NOT ANNOTATED YET)

---
+++ TeeTimeFinder/TeeTimeFinder/src/main/java/joseph/ciaravella/TeeTimeFinder/TeeTimeFinderApplication.java
@@ -0,0 +1,13 @@
+package joseph.ciaravella.TeeTimeFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeTimeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeTimeFinderApplication.class, args);
+	}
+
+}
---
+++ TeeTimeFinder/TeeTimeFinder/src/test/java/joseph/ciaravella/TeeTimeFinder/TeeTimeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package joseph.ciaravella.TeeTimeFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeTimeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
