0966fd83b71fa6b080b5b01f9ed995bf677e5ca1
Joseph Ciaravella <joseph.ciaravella@gmail.com>
2024-08-26 16:17:10 -0400

    added associated course field to course admin

--- TeeTimeFinder/src/main/java/joseph/ciaravella/TeeTimeFinder/model/CourseAdminAccount.java
+++ TeeTimeFinder/src/main/java/joseph/ciaravella/TeeTimeFinder/model/CourseAdminAccount.java
@@ -7,11 +7,25 @@ import jakarta.persistence.*;
 public class CourseAdminAccount extends UserAccount
 {
 
+  private String associatedCourse;
+
   public CourseAdminAccount() { super(); }
 
-  public CourseAdminAccount(String aEmail, String aPassword)
+  public CourseAdminAccount(String aEmail, String aPassword, String aAssociatedCourse)
   {
     super(aEmail, aPassword);
+    this.associatedCourse = aAssociatedCourse;
+  }
+
+  public boolean setAssociatedCourse(String aAssociatedCourse) {
+    boolean wasSet = false;
+    this.associatedCourse = aAssociatedCourse;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getAssociatedCourse() {
+    return this.associatedCourse;
   }
 
   public void delete()
